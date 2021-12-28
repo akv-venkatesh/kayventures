79e37c19edc3ce706323cdff04624efe96c2258a
anandkumar008 <anandkumarabserve@gmail.com>
2021-12-28 13:47:28 +0530

    api integration made for kay admin login register

--- src/store/saga.tsx
+++ src/store/saga.tsx
@@ -1,12 +1,16 @@
 import { all, fork } from 'redux-saga/effects';
 import postsSaga from '../actions/business_category/business_category_saga';
 import loginSaga from '../actions/login/login_saga';
+import adminregistersaga from '../actions/admin/registration_saga';
+import adminloginsaga from '../actions/admin/login_saga';
 
 export default function* () {
   
   yield all([
     loginSaga(),
-    postsSaga()
+    postsSaga(),
+    adminregistersaga(),
+    adminloginsaga(),
   ]);
   
 }
