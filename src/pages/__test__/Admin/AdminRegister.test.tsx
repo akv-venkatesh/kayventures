945bc5c4cecadc59b40eb9d7264cea68d40060ce
anandkumar008 <anandkumarabserve@gmail.com>
2022-01-03 19:42:17 +0530

    product configuration UI updated test completed

--- src/pages/__test__/Admin/AdminRegister.test.tsx
+++ src/pages/__test__/Admin/AdminRegister.test.tsx
@@ -12,14 +12,17 @@ const setup = (initialState={})=>{
 beforeEach(() => {
     wrapper = setup({});
 });
+
 test('register form test',()=>{
-    const firstName = wrapper.getByLabelText('First Name')
-    const MiddleName = wrapper.getByLabelText('Middle Name')
-    const lastname = wrapper.getByLabelText('Last Name')
-    const email = wrapper.getByLabelText('Email Address')
-    const phonenumber = wrapper.getByLabelText('Phone Number')
-    const password = wrapper.getByLabelText('New Password')
-    const confpassword = wrapper.getByLabelText('Confirm Password')
+    const firstName = wrapper.getByLabelText('First Name');
+    const MiddleName = wrapper.getByLabelText('Middle Name');
+    const lastname = wrapper.getByLabelText('Last Name');
+    const email = wrapper.getByLabelText('Email Address');
+    const phonenumber = wrapper.getByLabelText('Phone Number');
+    const password = wrapper.getByLabelText('New Password');
+    const confpassword = wrapper.getByLabelText('Confirm Password');
+    const textinputCount = wrapper.getAllByRole('textbox');
+    const pswinputCount = wrapper.getAllByPlaceholderText('Password');
     expect(firstName).toBeInTheDocument;
     expect(MiddleName).toBeInTheDocument;
     expect(lastname).toBeInTheDocument;
@@ -27,14 +30,17 @@ test('register form test',()=>{
     expect(phonenumber).toBeInTheDocument;
     expect(password).toBeInTheDocument;
     expect(confpassword).toBeInTheDocument;
-    const textinputCount = wrapper.getAllByRole('textbox');
     expect(textinputCount).toHaveLength(5);
-    const pswinputCount = wrapper.getAllByPlaceholderText('Password');
     expect(pswinputCount).toHaveLength(2);
+})
+
+//fireevents
+
+test('on submit the form and on change the input',() =>{
     const submitbtn = wrapper.getByRole('button', {name: /save/i});
+    const firstName = wrapper.getByLabelText('First Name');
     expect(submitbtn).toBeInTheDocument;
     fireEvent.click(submitbtn);
-    // expect(wrapper.findByText('sasd')).toBeInTheDocument;
     fireEvent.change(firstName, { target: { value: "anand" } });
     expect(wrapper.getByDisplayValue(/anand/i)).toBeInTheDocument;
 })
