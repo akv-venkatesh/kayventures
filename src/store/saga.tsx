8e86da820a3e5b190d636246d42355a22a028c78
IndrakumarR <indra@thegang.in>
2022-01-04 10:47:31 +0530

    Commit for separating Admin Module

--- src/store/saga.tsx
+++ src/store/saga.tsx
@@ -1,22 +1,13 @@
 import { all, fork } from 'redux-saga/effects';
 import postsSaga from '../actions/business_category/business_category_saga';
 import loginSaga from '../actions/login/login_saga';
-import adminregistersaga from '../actions/admin/registration_saga';
-import adminloginsaga from '../actions/admin/login_saga';
-import adminInvite from '../actions/admin/invite_saga';
-import adminpasswordChange from '../actions/admin/passwordchange_saga';
-import adminEditProfileSaga from '../actions/admin/editprofile.saga';
 
 export default function* () {
   
   yield all([
     loginSaga(),
-    postsSaga(),
-    adminregistersaga(),
-    adminloginsaga(),
-    adminInvite(),
-    adminpasswordChange(),
-    adminEditProfileSaga(),
+    postsSaga()
+
   ]);
   
 }
