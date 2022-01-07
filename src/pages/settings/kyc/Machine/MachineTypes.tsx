1ae308d8040a1efcbe1288611067e560aa9f5aac
vijayaraj.s <vijayaraj.s@abserve.tech>
2022-01-07 10:07:21 +0530

    machinerytype

--- src/pages/settings/kyc/Machine/MachineTypes.tsx
+++ src/pages/settings/kyc/Machine/MachineTypes.tsx
@@ -3,21 +3,27 @@ import { Col } from 'react-bootstrap';
 
 import MachineIcon from "../../../../assets/icons/various/MachineIcon.svg"
 
-class MachineTypes extends Component {
+interface typeProps {
+    machineTypeProps: any,
+    machineBrandProps: any,
+    machineTechProps: any,
+    machineCountProps: any,
+}
+class MachineTypes extends Component<typeProps, {}> {
     render() {
         return (
             <Col xs={3} md={12} className="column d-flex" >
-                {/* <div className="machine_items">
+                <div className="machine_items">
                     <div className="machine_image d-flex">
                         <img src={MachineIcon} alt="" />
-                        <h3>{this.props.machineCount}</h3>
+                        <h3>{this.props.machineCountProps}</h3>
                     </div>
-                    <p>{this.props.state.machineType}</p>
+                    <p>{this.props.machineTypeProps}</p>
                 </div>
                 <div className="machine_items_text">
-                    <p>{this.state.brandType}</p>
-                    <p>{this.state.typeTech}</p>
-                </div> */}
+                    <p>{this.props.machineBrandProps}</p>
+                    <p>{this.props.machineTechProps}</p>
+                </div>
             </Col>
         );
     }
