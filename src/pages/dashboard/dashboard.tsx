a4464a4d35a11e21c1c616f252fa18dfe7334898
anandkumar008 <anandkumarabserve@gmail.com>
2022-01-11 13:49:06 +0530

    dashboard files

---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import '../m-login.scss';
+
+class Dashboard extends Component{
+	render(){
+		return (
+			<div className='d-flex m-dashboard'>
+                
+            </div>
+		);
+	}
+}
+export default Dashboard;
