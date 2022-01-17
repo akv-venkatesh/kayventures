96a277eb44464a7293a505d5088c701ff61336b0
anandkumar008 <anandkumarabserve@gmail.com>
2022-01-17 19:04:35 +0530

    folder structure changes made

--- src/Routes/route.tsx
+++ src/Routes/route.tsx
@@ -8,19 +8,15 @@ import {
   PrimaryDetails,
   VerifiedEmail,
   VerifyEmailPending,
-  ProConfig,
   Types,
   Home,
-  ProConfigCapacity,
-  KYC, Organization, Organizationinfo, OrganizationinfoModal, OrganizationinfoModal1, OrganizationinfoModalSyn, Facilitykyc,
-  Facility1, Facilityday, Facilitymodal, Productconfig, Facilityform, Facilityinfomodal,
-  Pro_config,
-  MachineryInitialPage,
-  Esg,
+  Productselect, Facilitykyc,
+  Facility1, Facilityday, Facilitymodal, Facilityform, Facilityinfomodal,
+  Machinery,
   Settings,
   Dashboard,
   DashboardIndex,
-
+  OrgKYC,
   Operation, Profile,
 
 } from "./asyncpages";
@@ -39,11 +35,8 @@ class Routers extends React.Component {
           <Route path="/verifyemailpending" element={<VerifyEmailPending />} />
           <Route path='/login' element={<Login />} />
           <Route path='/home' element={<Home />} />
-          <Route path='/productConfiguration' element={<Productconfig />} />
-          <Route path='/product-config' element={<Pro_config />} />
 
           <Route path='/settings' element={<Settings />} >
-            {/* <Route path="kyc" element={<KYC />} /> */}
             {/* <Route path="proconfig" element={<ProConfig />} /> */}
             {/* <Route path="Capacity" element={<ProConfigCapacity />} /> */}
             {/* <Route path="productconfiguration" element={<Productconfig />} /> */}
@@ -67,18 +60,18 @@ class Routers extends React.Component {
             </Route>
 
             <Route path="kyc">
-              <Route path="organization-kyc">
-                <Route index element={<Organization />} />
-                <Route path="product-selection" element={<Productconfig/>} />
+              <Route path="organization">
+                <Route index element={<OrgKYC />} />
+                <Route path="product-selection" element={<Productselect/>} />
                 <Route path="machinery" element={<Machinery/>} />
-                <Route path="operation" element={<Operation />} />
+                {/* <Route path="operation" element={<Operation />} /> */}
               </Route>
               <Route path="facility">
-                <Route index element={<Facilitykyc />} />
+                {/* <Route index element={<Facilitykyc />} />
                 <Route path="operation" element={<Operation />} />
                 <Route path="product-selection" element={<Productconfig/>} />
                 <Route path="capacity" element={<ProConfigCapacity />} />
-                <Route path="section" element={<Facilityform />} />
+                <Route path="section" element={<Facilityform />} /> */}
               </Route>
             </Route>
           </Route>
